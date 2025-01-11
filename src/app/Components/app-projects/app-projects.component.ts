c4fa947204a0aab9f9d0a1a6233e58fc1981ff2f
Abigail Samanta Elis <86386981+abigailelis@users.noreply.github.com>
2025-01-11 18:28:09 -0300

    Update app-projects.component.ts

--- src/app/Components/app-projects/app-projects.component.ts
+++ src/app/Components/app-projects/app-projects.component.ts
@@ -10,6 +10,11 @@ export class AppProjectsComponent implements OnInit{
 
   projects: Project[] = [
   {
+      name: "Blog preview card challenge",
+      img: "/assets/Screenshot-card.PNG",
+      github: "https://github.com/abigailelis/git-test/tree/main/blog-preview-card"
+  },
+  {
       name: "QR code component challenge",
       img: "/assets/Screenshot.PNG",
       github: "https://github.com/abigailelis/git-test/tree/main/qr-code-component"
